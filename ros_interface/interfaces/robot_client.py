cd20feb842d7067582256de0a7d04cebd4b7de56
johannah <jh1736@gmail.com>
2020-01-30 14:04:28 -0500

    added test case

--- ros_interface/interfaces/robot_client.py
+++ ros_interface/interfaces/robot_client.py
@@ -1,8 +1,8 @@
 import socket
 import time
-
+from IPython import embed
 class RobotCommunicator():
-    def __init__(self, robot_ip="132.206.73.29", port=10003):
+    def __init__(self, robot_ip="capilano.cim.mcgill.ca", port=10003):
         self.robot_ip = robot_ip
         self.port = port
         self.connected = False
@@ -10,9 +10,9 @@ class RobotCommunicator():
 
     def connect(self):
         while not self.connected:
+            print("attempting to connect with robot at {}".format(self.robot_ip))
             self.tcp_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             # connect to computer
-            print("attempting to connect with robot at {}".format(self.robot_ip))
             self.tcp_socket.connect((self.robot_ip, self.port))
             print('connected')
             self.connected = True
@@ -31,7 +31,18 @@ class RobotCommunicator():
         self.connected = False
 
 if __name__ == '__main__':
-    rc = RobotCommunicator(robot_ip='127.0.0.1')
+    # works when client/server are both on local machine (capilano) 127.0.0.1
+    # success test case is:
+    # >> python robot_server.py # on capilano
+    # >> python robot_client.py 127.0.0.1 # on capilano
+    # fails when client/server are on different machines on lab subnet
+    # failure test case is:
+    # >> python robot_server.py # on capilano
+    # >> python robot_client.py 132.206.73.29 # on rhys
+    import sys
+    server_ip = sys.argv[1]
+    print('attempting to message server on %s - ensure it is running'%server_ip)
+    rc = RobotCommunicator(robot_ip=server_ip)
     rc.send('stuff')
     rc.send('stuff1')
     rc.send('stuff2')
