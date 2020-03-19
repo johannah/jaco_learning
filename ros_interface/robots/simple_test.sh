13c5cb9872b8480ee1ddc1ee3d5ed587de0766a5
johannah <jh1736@gmail.com>
2020-03-19 19:51:56 +0000

    added simple test routine

--- ros_interface/robots/simple_test.sh
+++ ros_interface/robots/simple_test.sh
@@ -1,3 +1,5 @@
+# home
+rosrun kinova_demo pose_action_client.py -v j2n7s300 mrad -- 0.212322831154 -0.257197618484 0.509646713734 1.63771402836 1.11316478252 0.134094119072
 # left
 rosrun kinova_demo pose_action_client.py -v j2n7s300 mdeg -- .62 -.31 .48 44.3 68.4 135.3 
 # move upwards 
@@ -6,3 +8,6 @@ rosrun kinova_demo pose_action_client.py -v j2n7s300 mdeg -- .42 -.31 .68 44.3 6
 rosrun kinova_demo pose_action_client.py -v j2n7s300 mdeg -- .12 -.41 .68 44.3 68.4 135
 # right
 rosrun kinova_demo pose_action_client.py -v j2n7s300 mdeg -- -.22 -.31 .68 44.3 68.4 135
+rosrun kinova_demo pose_action_client.py -v j2n7s300 mdeg -- -.22 -.31 .68 44.3 68.4 135
+# home
+rosrun kinova_demo pose_action_client.py -v j2n7s300 mrad -- 0.212322831154 -0.257197618484 0.509646713734 1.63771402836 1.11316478252 0.134094119072
